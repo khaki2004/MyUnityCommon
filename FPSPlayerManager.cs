3b2f71a3aeee78370c8c9b1a436720355dc6a748
khaki2004 <riku.khaki2004@gmail.com>
2025-07-08 21:41:56 +0900

    Update FPSPlayerManager.cs
    FPS視点更新。完成版

--- FPSPlayerManager.cs
+++ FPSPlayerManager.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
 
-public class FPSPlayerManager : MonoBehaviour
+public class PlayerManager : MonoBehaviour
 {
     private Rigidbody rb;
     private float moveSpeed = 10f;
     private float cameraSpeed = 3f;
     private float jumpPower = 4f;
     private bool isJump = false;
+    // クラス内に変数を追加（カメラ上下用）
+    private float verticalRotation = 0f;  // 上下視点の角度（X軸回転）
 
 
 
@@ -62,16 +64,27 @@ public class FPSPlayerManager : MonoBehaviour
 
         //右クリック、マウスドラッグでカメラの視点を操作する
         float x = Input.GetAxis("Mouse X") * cameraSpeed;
-        float y = Input.GetAxis("Mouse Y") * cameraSpeed;
+
+
+        //視点の上下操作
+        float y = Input.GetAxis("Mouse Y") * cameraSpeed; // 縦のマウス移動量
+
         if (Input.GetMouseButton(1))
         {
             if (Mathf.Abs(x) > 0.1f)
             {
                 transform.RotateAround(transform.position, Vector3.up, x);
             }
+
+            verticalRotation -= y; // 縦回転の角度に反映（上向きはマイナス）
+
+            // 真上（+90度）、真下（-90度）までの範囲に制限
+            verticalRotation = Mathf.Clamp(verticalRotation, -90f, 90f);
+            Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0f, 0f); // カメラを上下に回転
+                                                                                              // ↑↑上下設定ここまで ↑↑↑
         }
     }
-    
+
     //Groundに着地したらisJumpをfalseにする
     private void OnCollisionEnter(Collision collision)
     {
